387db2000844a86182df874ddda9a67aa0322b61
NewbieCreate <wkdtk85@daum.net>
2025-06-30 22:01:39 +0900

    Fetch: 묵시적 malloc free 기능구현

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -42,11 +42,122 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+/* Basic constants and macros */
+#define WSIZE 4
+#define DSIZE 8
+#define CHUCKSIZE (1<<12)
+
+#define MAX(x, y) ((x)>(y) ? (x) : (y))
+
+/* 합치기 */
+#define PACK(size, alloc) ((size)|(alloc))
+
+/* 주소 읽기 및 삽입 */
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+
+/* 사이즈 및 주소 얻기 */
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+/* 헤더 푸터 포인터 반환 */
+#define HDRP(bp) ((char *)(bp)-WSIZE)
+#define FTRP(bp) ((char *)(bp)+GET_SIZE(HDRP(bp)) - DSIZE)
+
+/* pre 블록 next 블록 위치 포인터 반환 */
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+/* 전역변수 선언 */
+static char *heap_listp;
+
+static void *coalesce(void *bp){
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if(prev_alloc && next_alloc) { /* case 1 */
+        return bp;
+    }else if(prev_alloc && !next_alloc){ /* case 2 */
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }else if(!prev_alloc && next_alloc) {/*case 3 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }else{/* case 4 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
+}
+
+static void *extend_heap(size_t words){
+    char *bp;
+    size_t size;
+
+    /* 가용블록 */
+    size = (words %2) ? (words+1) * WSIZE : words * WSIZE;
+    if((long)(bp = mem_sbrk(size)) == -1) return NULL;
+
+    /*  */
+    PUT(HDRP(bp), PACK(size, 0));   /*가용블록 헤더 */
+    PUT(FTRP(bp), PACK(size, 0));   /*가용블록 푸터 */
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0,1)); /* 새로운 에필로그 헤더 */
+
+    /*  */
+    return coalesce(bp);
+}
+
+static void place(void *bp, size_t asize){
+    size_t csize = GET_SIZE(HDRP(bp));
+
+    if((csize - asize) >= (2*DSIZE)){
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        bp = NEXT_BLKP(bp);
+        PUT(HDRP(bp), PACK(csize-asize, 0));
+        PUT(FTRP(bp), PACK(csize-asize, 0));
+    }else{
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
+
+}
+static void *find_fit(size_t asize){
+    /* fistfit search */
+    void *bp;
+
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) >0; bp = NEXT_BLKP(bp)){
+        if(!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))){
+            return bp;
+        }
+    }
+    return NULL; /* no fit */
+}
+
 /*
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    /* 비어있는 힙 생성 */
+    if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1) {
+        return -1;
+    }
+    PUT(heap_listp, 0);                          /*패딩 */
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1)); /*프롤로그 헤더*/
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));/*프롤로그 푸터*/
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1)); /* 에필로그 해더 */
+    heap_listp += (2*WSIZE);
+
+    /* 최소 힙 사이즈 구현 */
+    if(extend_heap(CHUCKSIZE/WSIZE) == NULL) {
+        return -1;
+    }
     return 0;
 }
 
@@ -56,22 +167,45 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
+    size_t asize;
+    size_t extendsize;
+    char *bp;
+
+    /* */
+    if(size == 0) {
         return NULL;
-    else
-    {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
     }
+
+    /*  */
+    if(size < DSIZE) {
+        asize = 2*DSIZE;
+    }else{
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+    }
+
+    /*  */
+    if ((bp = find_fit(asize)) != NULL) {
+        place(bp, asize);
+        return bp;
+    }
+    /* 찾을 수 없을 때 */
+    extendsize = MAX(asize, CHUCKSIZE);
+    if((bp = extend_heap(extendsize/ WSIZE)) == NULL){
+        return NULL;
+    }
+    place(bp, asize);
+    return bp;
 }
 
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void *ptr)
+void mm_free(void *bp)
 {
+    size_t size = GET_SIZE(HDRP(bp));
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    coalesce(bp);
 }
 
 /*
